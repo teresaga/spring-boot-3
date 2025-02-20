95df29c2006f8e7b5f7bd8de6b8d82c10a463254
Teresa <teresagalaviza@gmail.com>
2025-02-19 21:41:31 -0700

    First Commit

---
+++ src/main/java/org/example/myapp2/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package org.example.myapp2.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
