e1e95a60bd45e70e7a2cd6f96afbd30884782e0f
Teresa <teresagalaviza@gmail.com>
2025-02-20 16:42:32 -0700

    custom properties and endpoint to show properties added

--- src/main/java/org/example/myapp/rest/FunRestController.java
+++ src/main/java/org/example/myapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package org.example.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("{coach.name}")
+    private String coachName;
+    @Value("{team.name}")
+    private String teamName;
+
+    // expose new endpoint for "team-info"
+    @GetMapping("team-info")
+    public String teamInfo() {
+        return "Coach: " + coachName + " , Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String hello() {
